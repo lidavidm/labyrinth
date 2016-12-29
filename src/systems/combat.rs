9e2a3ab439328ceccd685f8fbba188276f115b4f
David Li <li.davidm96@gmail.com>
2016-12-28 20:24:40 -0700

    fix(Combat): remove entity from collision map after death

--- src/systems/combat.rs
+++ src/systems/combat.rs
@@ -3,7 +3,7 @@ use std::sync::mpsc;
 use rand::{self, Rng};
 use specs::{self, Join};
 
-use ::components::{ai, combat, health};
+use ::components::{ai, combat, health, map, position};
 
 pub struct CombatSystem {
     message_queue: mpsc::Sender<String>,
@@ -19,20 +19,23 @@ impl CombatSystem {
 
 impl specs::System<()> for CombatSystem {
     fn run(&mut self, arg: specs::RunArg, _: ()) {
-        let (entities, mut chasers, mut attacked, mut healths) = arg.fetch(|world| {
+        let (entities, mut map, mut chasers, mut attacked, mut healths, positions) = arg.fetch(|world| {
             (
                 world.entities(),
+                world.write_resource::<map::Map>(),
                 world.write::<ai::ChaseBehavior>(),
                 world.write::<combat::Attack>(),
                 world.write::<health::Health>(),
+                world.read::<position::Position>(),
             )
         });
 
         let mut to_delete = vec![];
-        for (entity, attack, health) in (&entities, &mut attacked, &mut healths).iter() {
+        for (entity, attack, health, position) in (&entities, &mut attacked, &mut healths, &positions).iter() {
             let damage = rand::thread_rng().gen_range(attack.damage.0, attack.damage.1);
             if damage >= health.health {
                 self.message_queue.send(format!("Hit for {} damage, killed!", damage)).unwrap();
+                map.vacate(position.x, position.y);
                 arg.delete(entity);
             }
             else {
