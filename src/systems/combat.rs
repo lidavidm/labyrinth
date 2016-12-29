81f4fe6ae9023f2c9203b635add2a2591f836fcb
David Li <li.davidm96@gmail.com>
2016-12-28 20:22:17 -0700

    feat(Combat): damage entity on hit

--- src/systems/combat.rs
+++ src/systems/combat.rs
@@ -3,7 +3,7 @@ use std::sync::mpsc;
 use rand::{self, Rng};
 use specs::{self, Join};
 
-use ::components::{combat, health};
+use ::components::{ai, combat, health};
 
 pub struct CombatSystem {
     message_queue: mpsc::Sender<String>,
@@ -19,9 +19,10 @@ impl CombatSystem {
 
 impl specs::System<()> for CombatSystem {
     fn run(&mut self, arg: specs::RunArg, _: ()) {
-        let (entities, mut attacked, mut healths) = arg.fetch(|world| {
+        let (entities, mut chasers, mut attacked, mut healths) = arg.fetch(|world| {
             (
                 world.entities(),
+                world.write::<ai::ChaseBehavior>(),
                 world.write::<combat::Attack>(),
                 world.write::<health::Health>(),
             )
@@ -30,7 +31,15 @@ impl specs::System<()> for CombatSystem {
         let mut to_delete = vec![];
         for (entity, attack, health) in (&entities, &mut attacked, &mut healths).iter() {
             let damage = rand::thread_rng().gen_range(attack.damage.0, attack.damage.1);
-            self.message_queue.send(format!("Hit for {} damage", damage)).unwrap();
+            if damage >= health.health {
+                self.message_queue.send(format!("Hit for {} damage, killed!", damage)).unwrap();
+                arg.delete(entity);
+            }
+            else {
+                // TODO: if not player, add chase behavior
+                health.health -= damage;
+                self.message_queue.send(format!("Hit for {} damage, {} left", damage, health.health)).unwrap();
+            }
             to_delete.push(entity);
         }
 
