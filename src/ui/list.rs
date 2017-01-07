cb5a27f9495676fdfac26e40129d381b4b8870f2
David Li <li.davidm96@gmail.com>
2017-01-07 09:25:59 -0700

    feat(UI): implement List widget

--- src/ui/list.rs
+++ src/ui/list.rs
@@ -1,11 +1,15 @@
-use voodoo::window::Point;
+use voodoo::color::ColorValue;
+use voodoo::window::{Point, Window};
+
+use super::ColorPair;
 
 pub struct List<T> {
     pub contents: Vec<T>,
     pub cursor: usize,
-    pub bounds: (Point, Point),
-    pub normal: super::ColorPair,
-    pub highlight: super::ColorPair,
+    pub position: Point,
+    pub bounds: (u16, u16),
+    pub normal: ColorPair,
+    pub highlight: ColorPair,
 }
 
 pub trait ListRenderable {
@@ -13,5 +17,57 @@ pub trait ListRenderable {
 }
 
 impl<T: ListRenderable> List<T> {
+    pub fn new(position: Point, width: u16, height: u16) -> List<T> {
+        // TODO: check that we have enough height to render everything
+        List {
+            contents: Vec::new(),
+            cursor: 0,
+            position: position,
+            bounds: (position.x + width, position.y + height),
+            normal: ColorPair::new(ColorValue::White, ColorValue::Black),
+            highlight: ColorPair::new(ColorValue::Black, ColorValue::White),
+        }
+    }
 
+    pub fn move_cursor_up(&mut self) {
+        if self.cursor > 0 {
+            self.cursor -= 1;
+        }
+    }
+
+    pub fn move_cursor_down(&mut self) {
+        if self.cursor < self.contents.len() - 1 {
+            self.cursor += 1;
+        }
+    }
+
+    pub fn get_selected(&self) -> Option<&T> {
+        self.contents.get(self.cursor)
+    }
+
+    pub fn refresh(&self, window: &mut Window) {
+        // TODO: account for our width
+        if self.cursor > 0 {
+            window.print_at(self.position, "...");
+        }
+
+        let mut rendered = 1;
+        let mut early_break = false;
+        for item in &self.contents[self.cursor..self.contents.len()] {
+            let desc = item.render();
+            if rendered + desc.len() >= self.bounds.1 as usize {
+                early_break = true;
+                break;
+            }
+
+            for line in desc {
+                window.print_at(Point::new(self.position.x, self.position.y + rendered as u16), &line);
+                rendered += 1;
+            }
+        }
+
+        if early_break {
+            window.print_at(Point::new(self.position.x, self.position.y + rendered as u16), "...");
+        }
+    }
 }
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,6 +1,19 @@
 use voodoo::color::ColorValue;
 
+pub mod list;
+
+pub use self::list::{List, ListRenderable};
+
 pub struct ColorPair {
     pub fg: ColorValue,
     pub bg: ColorValue,
+}
+
+impl ColorPair {
+    pub fn new(fg: ColorValue, bg: ColorValue) -> ColorPair {
+        ColorPair {
+            fg: fg,
+            bg: bg,
+        }
+    }
 }
